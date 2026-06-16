558e3ab7edaaa760e26b58eac3ee89a9dc26a848
John <john.chavez113@gmail.com>
2026-06-15 23:39:28 -0400

    Added due date and SLA calculations

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,34 @@
 from fastapi import FastAPI
 from pathlib import Path
+from datetime import datetime, timedelta
+
 import pandas as pd
 app = FastAPI()
 
 dataFile = Path(__file__).parent.parent / "data" / "work_orders.csv"
+dateFormat = "%Y-%m-%d %H:%M"
+
+slaRules = {
+    "P1": timedelta(hours=4),
+    "P2": timedelta(hours=24),
+    "P3": timedelta(hours=48),
+    "P4": timedelta(days=5),
+    "P5": timedelta(days=10),
+    "P6": timedelta(days=60),
+    }
+
+def calculateDueDate(created_date: str, priority: str) ->str:
+    created = datetime.strptime(created_date, dateFormat)
+    slaDuration = slaRules[priority]
+    dueDate = created + slaDuration
+    return dueDate.strftime(dateFormat)
 
 def loadWorkOrders() -> list[dict]:
     df = pd.read_csv(dataFile)
     workOrders = df.to_dict(orient="records")
+
+    for workOrder in workOrders:
+        workOrder["dueDate"] = calculateDueDate(workOrder["created_date"],workOrder["priority"])
 
     return workOrders
 @app.get("/")
